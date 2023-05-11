65c0f888f8779e08f3d55d060a4395dfb82a9606
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-12 01:18:27 +0300

    use the string literal approach Instead of collecting 2 strings (string + string)

--- src/pages/Login/Paragraph/Paragraph.js
+++ src/pages/Login/Paragraph/Paragraph.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Paragraph = ({ textParagraph , textLink, styleParagraph , signUpLinkProps }) => {
+const Paragraph = ({ textParagraph, textLink, styleParagraph, signUpLinkProps }) => {
+  const concatenatedText = `${textParagraph} ${' '}`;
+
   return (
-    <p className={styleParagraph }>
-      {textParagraph }{' '}
+    <p className={styleParagraph}>
+      {concatenatedText}
       <Link {...signUpLinkProps}>
         {textLink}
       </Link>
