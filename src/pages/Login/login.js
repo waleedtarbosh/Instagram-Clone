dabde4b630628cd59e7d8e7c525c88f3d8cb6113
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-11 23:08:04 +0300

    I deleted the logo component because it is almost identical to the image component

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,17 +1,26 @@
-import Loader from '../../Components/Loader/Loader';
-import Image from './Image/Image'
-import Logo from './Logo/Logo';
+import Loader from "../../Components/Loader/Loader";
+import Image from "./Image/Image";
+
 export default function Login() {
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-       <Loader fadeOutInterval={5000}/>
-       <Image src="/images/iphone-with-profile.jpg"  alt="iPhone with Instagram app" styleImg='flex w-3/5' />
-       <div className="flex flex-col w-2/5">
+      <Loader fadeOutInterval={5000} />
+      <Image
+        src="/images/iphone-with-profile.jpg"
+        alt="iPhone with Instagram app"
+        styleImg="flex w-3/5"
+      />
+      <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-          
-        <Logo src="/images/logo.png" alt="Instagram" style1="flex justify-center w-full"  style2="mt-2 w-6/12 mb-4" />
+          <h1 className="flex justify-center w-full">
+            <Image
+              src="/images/logo.png"
+              alt="Instagram"
+              styleImg="mt-2 w-6/12 mb-4"
+            />
+          </h1>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
