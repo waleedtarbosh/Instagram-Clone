38bd1b716cf430d3768ce6a0e4fd283b4ed54224
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-12 00:14:43 +0300

    Use single CustomInput with array data and unique keys

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -8,17 +8,38 @@ export default function Login() {
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  const inputFields = [
+    {
+      id: "email",
+      label: "Email address",
+      type: "text",
+      placeholder: "Enter your Email Address",
+      value: emailAddress,
+      onChange: (event) => setEmailAddress(event.target.value)
+    },
+    {
+      id: "password",
+      label: "Password",
+      type: "password",
+      placeholder: "Enter your Password",
+      value: password,
+      onChange: (event) => setPassword(event.target.value)
+    }
+  ];
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
     try {
+      //login logic here
     } catch (error) {
       setEmailAddress("");
       setPassword("");
       setError(error.message);
     }
   };
-  
+
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       <Loader fadeOutInterval={5000} />
@@ -37,22 +58,18 @@ export default function Login() {
           />
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
           <form onSubmit={handleLogin} method="POST">
-            <CustomInput
-              aria-label="Enter your email address"
-              type="text"
-              placeholder="Email address"
-              styleInput="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setEmailAddress(target.value)}
-              value={emailAddress}
-            />
-            <CustomInput
-              aria-label="Enter your password"
-              type="password"
-              placeholder="Password"
-              styleInput="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setPassword(target.value)}
-              value={password}
-            />
+            {inputFields.map((input) => (
+              <CustomInput
+                key={input.id}
+                id={input.id}
+                aria-label={input.label}
+                type={input.type}
+                placeholder={input.placeholder}
+                styleInput="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                onChange={input.onChange}
+                value={input.value}
+              />
+            ))}
           </form>
         </div>
       </div>
