e549e0a46507cea78d5356a842b3c8ca08770949
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-14 13:24:51 +0300

    Implement signup page

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,11 +1,12 @@
-import Loader from "../../Components/Loader/Loader";
-import CustomInput from "./CustomInput/CustomInput";
-import React, { useState} from "react";
-import Image from "./Image/Image";
-import CustomButton from "./CustomButton/CustomButton";
-import Paragraph from "./Paragraph/Paragraph";
+import CustomInput from "../Login/CustomInput/CustomInput";
+import { useState } from "react";
+import Image from "../Login/Image/Image";
+import CustomButton from "../Login/CustomButton/CustomButton";
+import Paragraph from "../Login/Paragraph/Paragraph";
 
-export default function Login() {
+export default function SignUp() {
+  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -22,6 +23,30 @@ export default function Login() {
     imageWrapperStyle: "mt-2 w-6/12 mb-4",
   };
   const inputFields = [
+    
+    {
+        id: "Username",
+        label:"Enter your username",
+        type: "text",
+        placeholder: "Username",
+        styleInput:
+        "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2",
+        onChange: (event) => setUsername(event.target.value),
+        value: username,
+
+    },
+
+    { 
+    id: "FullName",
+    label:"Enter your full name",
+    type: "text",
+    placeholder: "Full name",
+    styleInput:
+    "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2",
+        onChange: (event) => setFullName(event.target.value),
+        value: fullName,
+    },
+
     {
       id: "email",
       label: "Email address",
@@ -42,6 +67,7 @@ export default function Login() {
       value: password,
       onChange: (event) => setPassword(event.target.value),
     },
+
   ];
   const buttonProps = {
     disabled: isInvalid,
@@ -49,15 +75,15 @@ export default function Login() {
     styleButton: `bg-blue-medium text-white w-full rounded h-8 font-bold ${
       isInvalid && "opacity-50"
     }`,
-    content: "Login",
+    content: "SignUp",
   };
   const ParagraphProps = {
-    text: "Don't have an account?",
-    linkText: "Sign up",
+    text: "Have an account?",
+    linkText: "Login",
     textStyle: "text-sm",
   };
   const linkProps = {
-    to: "/Signup",
+    to: "/login",
     className: "font-bold text-blue-medium",
     type: "submit",
     styleButton: `bg-blue-medium text-white w-full rounded h-8 font-bold ${
@@ -65,21 +91,21 @@ export default function Login() {
     }`,
   };
 
-  const handleLogin = async (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault();
 
     try {
-        // Login logic here
+      //login logic here
     } catch (error) {
-      setEmailAddress("");
-      setPassword("");
-      setError(error.message);
+        setFullName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
     }
   };
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <Loader fadeOutInterval={5000} />
       <Image {...ImageProps} />
       <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
@@ -87,7 +113,7 @@ export default function Login() {
             <Image {...LogoProps} />
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignUp} method="POST">
             {inputFields.map((input) => (
               <CustomInput key={input.id} {...input} />
             ))}
