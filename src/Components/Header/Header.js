2c1f8b7b9f1c2e3990207e64817e542bf58c241a
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-21 15:32:30 +0300

    Renamed the variable PathProps2 to logoutIconProps to make it more descriptive and self-explanatory.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ import PathComponent from "./PathComponent/PathComponent";
 const instagramLogo = {
   src: "/images/logo.png",
   alt: "Instagram",
-  imageWrapperStyle: "mt-2 w-6/12",
+  imageWrapperStyle: "mt-2 w-6/12"
 };
 
 const svgProps = {
@@ -16,27 +16,27 @@ const svgProps = {
   xmlns: "http://www.w3.org/2000/svg",
   fill: "none",
   viewBox: "0 0 24 24",
-  stroke: "currentColor",
+  stroke: "currentColor"
 };
 
 const homeIconProps = {
   strokeLinecap: "round",
   strokeLinejoin: "round",
   strokeWidth: 2,
-  d: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6",
+  d: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
 };
 
-const pathProps2 = {
+const logoutIconProps = {
   strokeLinecap: "round",
   strokeLinejoin: "round",
   strokeWidth: 2,
-  d: "M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1",
+  d: "M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
 };
 const profileImageProps = {
   src: "/images/avatars/karl.jpg",
   alt: "img profile",
   imageWrapperStyle: "h-8 w-8 flex ",
-  imageStyle: "rounded-full",
+  imageStyle: "rounded-full"
 };
 
 export default function Header() {
@@ -58,7 +58,6 @@ export default function Header() {
                   <PathComponent {...homeIconProps} />
                 </SvgComponent>
               </Link>
-
               <button
                 type="button"
                 title="Sign Out"
@@ -72,7 +71,7 @@ export default function Header() {
                 }}
               >
                 <SvgComponent {...svgProps}>
-                  <PathComponent {...pathProps2} />
+                  <PathComponent {...logoutIconProps} />
                 </SvgComponent>
               </button>
               <div className="flex items-center cursor-pointer">
