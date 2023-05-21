08ff6e936f6904deded6ae91b1cbe3e2032952c1
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-21 15:18:32 +0300

    Fix indentation of code block between lines 78 and 82

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
-import Image from '../../pages/Login/Image/Image';
-import SvgComponent from './SvgComponent/SvgComponent';
-import PathComponent from './PathComponent/PathComponent';
+import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import Image from "../../pages/Login/Image/Image";
+import SvgComponent from "./SvgComponent/SvgComponent";
+import PathComponent from "./PathComponent/PathComponent";
 
 const Logo = {
-  src: '/images/logo.png',
-  alt: 'Instagram',
-  imageWrapperStyle: 'mt-2 w-6/12'
+  src: "/images/logo.png",
+  alt: "Instagram",
+  imageWrapperStyle: "mt-2 w-6/12",
 };
 
 const SvgProps = {
-  className: 'w-8 mr-6 text-black-light cursor-pointer',
-  xmlns: 'http://www.w3.org/2000/svg',
-  fill: 'none',
-  viewBox: '0 0 24 24',
-  stroke: 'currentColor'
+  className: "w-8 mr-6 text-black-light cursor-pointer",
+  xmlns: "http://www.w3.org/2000/svg",
+  fill: "none",
+  viewBox: "0 0 24 24",
+  stroke: "currentColor",
 };
 
 const PathProps1 = {
-  strokeLinecap: 'round',
-  strokeLinejoin: 'round',
+  strokeLinecap: "round",
+  strokeLinejoin: "round",
   strokeWidth: 2,
-  d: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+  d: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6",
 };
 
 const PathProps2 = {
-  strokeLinecap: 'round',
-  strokeLinejoin: 'round',
+  strokeLinecap: "round",
+  strokeLinejoin: "round",
   strokeWidth: 2,
-  d: 'M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1'
+  d: "M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1",
 };
 const ProfileImageProps = {
   src: "/images/avatars/karl.jpg",
   alt: "img profile",
   imageWrapperStyle: "h-8 w-8 flex ",
-  imageStyle: "rounded-full"
+  imageStyle: "rounded-full",
 };
 
 export default function Header() {
@@ -66,7 +66,7 @@ export default function Header() {
                   // Handle sign out logic here
                 }}
                 onKeyDown={(event) => {
-                  if (event.key === 'Enter') {
+                  if (event.key === "Enter") {
                     // Handle sign out logic here
                   }
                 }}
@@ -76,10 +76,10 @@ export default function Header() {
                 </SvgComponent>
               </button>
               <div className="flex items-center cursor-pointer">
-                    <Link to={`/p/karl`} >
+                <Link to={`/p/karl`}>
                   <Image {...ProfileImageProps}></Image>
-                    </Link>
-                  </div>
+                </Link>
+              </div>
             </>
           </div>
         </div>
