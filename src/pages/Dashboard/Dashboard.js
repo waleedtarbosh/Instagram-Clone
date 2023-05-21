3777dfd49ddea4c0e37ba2d6ff0c4747c1e9259c
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-21 15:36:40 +0300

    Refactor Dashboard component to use React fragment instead of unnecessary outer div

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import Header from '../../Components/Header/Header';
+
 export default function Dashboard() {
   return (
-    <div>
+    <>
       <div className="bg-gray-background">
         <Header />
       </div>
-    </div>
+    </>
   );
 }
