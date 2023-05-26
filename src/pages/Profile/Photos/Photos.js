8c0d8d36d6f50df9df9bd502655a56c055a14f98
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-26 17:30:37 +0300

    Refactor Photos component to use const variable for SvgPathComponent props

--- src/pages/Profile/Photos/Photos.js
+++ src/pages/Profile/Photos/Photos.js
@@ -16,8 +16,7 @@ const Photos = ({ photos }) => {
     strokeLinecap: "",
     strokeLinejoin: "",
     strokeWidth: 2,
-    d:
-      "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z",
+    d: "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z",
     fillRule: "evenodd",
     clipRule: "evenodd",
   };
@@ -26,8 +25,7 @@ const Photos = ({ photos }) => {
     strokeLinecap: "",
     strokeLinejoin: "",
     strokeWidth: 2,
-    d:
-      "M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z",
+    d: "M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z",
     fillRule: "evenodd",
     clipRule: "evenodd",
   };
@@ -36,6 +34,11 @@ const Photos = ({ photos }) => {
     imageWrapperStyle: "",
     imageStyle: "w-full h-full object-cover",
   };
+
+  const svgComponentProps = [
+    { svgProps: svgProps, pathProps: heartIconProps },
+    { svgProps: svgProps, pathProps: commentIconProps },
+  ];
 
   if (photos?.length > 0) {
     return (
@@ -47,14 +50,8 @@ const Photos = ({ photos }) => {
               <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex">
                 <div className="flex justify-center items-center">
                   <div className={styles["icons"]}>
-                    {[
-                      { svgProps, pathProps: heartIconProps },
-                      { svgProps, pathProps: commentIconProps },
-                    ].map((props) => (
-                      <p
-                        key={`${photo.id}-${props.pathProps.d}`}
-                        className="flex items-center text-white font-bold"
-                      >
+                    {svgComponentProps.map((props, index) => (
+                      <p key={photo.id + index} className="flex items-center text-white font-bold">
                         <SvgPathComponent {...props} />
                       </p>
                     ))}
