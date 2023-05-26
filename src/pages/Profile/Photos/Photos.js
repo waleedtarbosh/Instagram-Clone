4ca5c5d98a2775f9d622a6d73cbf334917d66a34
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-26 16:37:42 +0300

    Refactor Photos component to use array for SvgPathComponent props

--- src/pages/Profile/Photos/Photos.js
+++ src/pages/Profile/Photos/Photos.js
@@ -16,7 +16,8 @@ const Photos = ({ photos }) => {
     strokeLinecap: "",
     strokeLinejoin: "",
     strokeWidth: 2,
-    d: "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z",
+    d:
+      "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z",
     fillRule: "evenodd",
     clipRule: "evenodd",
   };
@@ -25,7 +26,8 @@ const Photos = ({ photos }) => {
     strokeLinecap: "",
     strokeLinejoin: "",
     strokeWidth: 2,
-    d: "M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z",
+    d:
+      "M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z",
     fillRule: "evenodd",
     clipRule: "evenodd",
   };
@@ -45,19 +47,17 @@ const Photos = ({ photos }) => {
               <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex">
                 <div className="flex justify-center items-center">
                   <div className={styles["icons"]}>
-                    <p className="flex items-center text-white font-bold">
-                      <SvgPathComponent
-                        svgProps={svgProps}
-                        pathProps={heartIconProps}
-                      />
-                    </p>
-
-                    <p className="flex items-center text-white font-bold">
-                      <SvgPathComponent
-                        svgProps={svgProps}
-                        pathProps={commentIconProps}
-                      />
-                    </p>
+                    {[
+                      { svgProps, pathProps: heartIconProps },
+                      { svgProps, pathProps: commentIconProps },
+                    ].map((props) => (
+                      <p
+                        key={`${photo.id}-${props.pathProps.d}`}
+                        className="flex items-center text-white font-bold"
+                      >
+                        <SvgPathComponent {...props} />
+                      </p>
+                    ))}
                   </div>
                 </div>
               </div>
