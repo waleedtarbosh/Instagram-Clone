66fc41b7b6e6bf7538bce64662ddd6ec82a6145d
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-26 16:25:10 +0300

    Combine PathComponent and PathIconsComponent into a single component

--- src/Components/Header/PathComponent/PathComponent.js
+++ src/Components/Header/PathComponent/PathComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const PathComponent = ({ strokeLinecap, strokeLinejoin, strokeWidth, d }) => {
+const PathComponent = ({ strokeLinecap, strokeLinejoin, strokeWidth, d,fillRule, clipRule }) => {
   return (
     <path
       strokeLinecap={strokeLinecap}
       strokeLinejoin={strokeLinejoin}
       strokeWidth={strokeWidth}
       d={d}
+      fillRule={fillRule} 
+      clipRule={clipRule}
     />
   );
 };
--- src/pages/Profile/Photos/Photos.js
+++ src/pages/Profile/Photos/Photos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Photos.module.css";
-import SvgPathIconsComponent from "./SvgPathIconsComponent/SvgPathIconsComponent";
+import SvgPathComponent from "../../../Components/Header/SvgPathComponent/SvgPathComponent";
 import Image from "../../Login/Image/Image";
 
 const Photos = ({ photos }) => {
@@ -13,14 +13,20 @@ const Photos = ({ photos }) => {
   };
 
   const heartIconProps = {
-    fillRule: "evenodd",
+    strokeLinecap: "",
+    strokeLinejoin: "",
+    strokeWidth: 2,
     d: "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z",
+    fillRule: "evenodd",
     clipRule: "evenodd",
   };
 
   const commentIconProps = {
-    fillRule: "evenodd",
+    strokeLinecap: "",
+    strokeLinejoin: "",
+    strokeWidth: 2,
     d: "M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z",
+    fillRule: "evenodd",
     clipRule: "evenodd",
   };
 
@@ -40,17 +46,17 @@ const Photos = ({ photos }) => {
                 <div className="flex justify-center items-center">
                   <div className={styles["icons"]}>
                     <p className="flex items-center text-white font-bold">
-                      <SvgPathIconsComponent
+                      <SvgPathComponent
                         svgProps={svgProps}
                         pathProps={heartIconProps}
                       />
                     </p>
 
                     <p className="flex items-center text-white font-bold">
-                      <SvgPathIconsComponent
+                      <SvgPathComponent
                         svgProps={svgProps}
                         pathProps={commentIconProps}
-                      />                      
+                      />
                     </p>
                   </div>
                 </div>
