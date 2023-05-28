a24e9b19869775eac95f24fe2c79b09f200a8c44
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-28 10:36:18 +0300

    Refactor FooterPosts component to use an array of objects for rendering username and caption

--- src/Components/Timeline/Posts/FooterPosts/FooterPosts.js
+++ src/Components/Timeline/Posts/FooterPosts/FooterPosts.js
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 
 export default function FooterPosts({ caption, username }) {
+  const usersComments  = [
+    { id: 'username', value: username, className: 'mr-1 font-bold' },
+    { id: 'caption', value: caption, className: 'italic' },
+  ];
+
   return (
     <div className="p-4 pt-2 pb-1">
-      <span className="mr-1 font-bold">{username}</span>
-      <span className="italic">{caption}</span>
+      {usersComments.map((element) => (
+        <span key={element.id} className={element.className}>
+          {element.value}
+        </span>
+      ))}
     </div>
   );
 }
