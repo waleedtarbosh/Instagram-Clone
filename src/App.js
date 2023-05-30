4db7a39e8b68a522d37f54e04c1b14be74527c44
waleedtarbosh <waleedtarbush@gmail.com>
2023-05-30 19:16:14 +0300

    Remove interval for loader and limit its usage to the login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lazy,Suspense } from "react";
+import { lazy, Suspense } from "react";
 import * as ROUTES from './constants/routes'
 import Layout from "./Components/Layout/Layout";
 import Loader from "./Components/Loader/Loader";
+
 const Login = lazy(() => import("./pages/Login/Login"));
 const SignUp = lazy(() => import('./pages/SignUp/SignUp'));
 const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard'));
@@ -15,17 +16,17 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      <Suspense fallback={<Loader fadeOutInterval={5000} />}>
-        <Routes>
-        <Route path={ROUTES.LOGIN} element={<Login />} />   
-        <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-        <Route element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path={ROUTES.PROFILE} element={<MyProfile />} />
-            <Route path={ROUTES.FRIENDS_PROFILE} element={<FriendsProfile/>} />
-            <Route path={ROUTES.SETTINGS} element={<Settings />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path={ROUTES.PROFILE} element={<MyProfile />} />
+              <Route path={ROUTES.FRIENDS_PROFILE} element={<FriendsProfile />} />
+              <Route path={ROUTES.SETTINGS} element={<Settings />} />
+            </Route>
+          </Routes>
         </Suspense>
       </BrowserRouter>
     </div>
