60efda74b393994897ff6cddcfc4e80b55062759
waleedtarbosh <waleedtarbush@gmail.com>
2023-06-14 19:48:01 +0300

    Add Firebase integration to Suggestions and SuggestedProfile.js components

--- src/Components/Sidebar/SuggestedProfile.js
+++ src/Components/Sidebar/SuggestedProfile.js
@@ -1,25 +1,49 @@
 import { Link } from 'react-router-dom';
 import Image from '../../pages/Login/Image/Image';
-
-export default function SuggestedProfile({ profile }) {
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  updateLoggedInUserFollowing,
+  updateFollowedUserFollowers,
+  getUserByUserId
+} from '../../services/firebase';
+export default function SuggestedProfile({profileDocId,username,profileId,userId,loggedInUserDocId}) {
   const ProfileImageProps = {
-    src: `/images/avatars/${profile.username}.jpg`,
+    src: `/images/avatars/${username}.jpg`,
     alt: "img profile",
     imageWrapperStyle: "w-8 flex mr-3",
     imageStyle: "rounded-full"
   };
-
-  return (
+  const [followed, setFollowed] = useState(false);
+  async function handleFollowUser() {
+    setFollowed(true);
+    await updateLoggedInUserFollowing(loggedInUserDocId, profileId, false);
+    await updateFollowedUserFollowers(profileDocId, userId, false);
+    
+    
+  }
+  return !followed ? (
     <div className="flex flex-row items-center align-items justify-between">
       <div className="flex items-center justify-between">
         <Image {...ProfileImageProps} />
-        <Link to={`/p/${profile.username}`}>
-          <p className="font-bold text-sm">{profile.username}</p>
+        <Link to={`/p/${username}`}>
+          <p className="font-bold text-sm">{username}</p>
         </Link>
       </div>
-      <button className="text-xs font-bold text-blue-medium" type="button">
+      <button
+        className="text-xs font-bold text-blue-medium"
+        type="button"
+        onClick={handleFollowUser}
+      >
         Follow
       </button>
     </div>
-  );
+      ) : null;
 }
+SuggestedProfile.propTypes = {
+  profileDocId: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  profileId: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired,
+  loggedInUserDocId: PropTypes.string.isRequired
+};
